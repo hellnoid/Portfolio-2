a1d3e963c8e1c083538bc40bff04dc6e218621c8
hellnoid <xainhellnoid22@gmail.com>
2022-08-21 14:02:45 +0530

    yo

--- src/js/typed.js
+++ src/js/typed.js
@@ -1,11 +1,11 @@
-import Typed from 'typed.js';
+import Typed from "typed.js";
 
 var options = {
-  strings: ["I'm a Front-end Developer", 'I Build  Websites'],
+  strings: ["I'm a Fullstack Developer", "I Build  Websites"],
   typeSpeed: 120,
   backSpeed: 50,
-  backDelay: 1500
+  backDelay: 1500,
   // loop: true
 };
 
-var typed = new Typed('#headline', options);
+var typed = new Typed("#headline", options);
